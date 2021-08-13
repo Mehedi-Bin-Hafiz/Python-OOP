9ad25d5a129dec3dc64f895deae97ba72bc63229
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2021-08-13 18:21:18 +0600

    function all complete

---
+++ function/callableTestModule.py
@@ -0,0 +1,8 @@
+def fullName(firstName,LastName):
+    fullName = firstName+" "+LastName
+    print(fullName.title())
+def sumFunction(a,b):
+    return  a+b
+if __name__ == '__main__':
+    fullName('mehedi',LastName='Hasan')
+
---
+++ function/importingEntireModule.py
@@ -0,0 +1,20 @@
+import callableTestModule as CT
+
+CT.fullName('Hasan','Mehedi')
+
+##import specific funciton
+#from module_name import function_name
+from function.callableTestModule import sumFunction
+
+result = sumFunction(3,5)
+print(result)
+
+#improt allfunction
+#from modulename import *
+
+
+
+
+
+
+
