6ca72a42f7ef3c51578440307b23d36d0076cd46
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2021-08-13 11:19:15 +0600

    how to return value from a function

---
+++ function/ReturnValues.py
@@ -0,0 +1,13 @@
+def get_formatted_name(first_name, last_name):
+    """Return a full name, neatly formatted."""
+    full_name = first_name + ' ' + last_name
+    return full_name.title()
+musician = get_formatted_name('HAHA', 'hendrix')
+print(musician)
+
+def get_formatted_name(first_name, last_name,middle_name = ''):
+    """Return a full name, neatly formatted."""
+    full_name = first_name + middle_name+ ' ' + last_name
+    return full_name.title()
+musician = get_formatted_name('HAHA', 'hendrix')
+print(musician)
