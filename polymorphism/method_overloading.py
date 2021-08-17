2c0e79574d81e2f8b942d8a52d25b3e5db90df35
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2021-08-17 16:59:07 +0600

    mehtod overloading

---
+++ polymorphism/method_overloading.py
@@ -0,0 +1,23 @@
+
+# python discourage it
+
+class Car():
+
+    def __init__(self,v1,v2):
+        self.v1 = v1
+        self.v2 = v2
+
+    def noah(self):
+        print('this is noah with', self.v1)
+    def noah(self):
+        print('this is noah with', self.v2)
+
+car = Car('gas', 'bettery')
+
+car.noah()
+car.noah()
+
+##### Method overloading means when we create same method with differet parameter then is called. python does not support it
+
+
+
