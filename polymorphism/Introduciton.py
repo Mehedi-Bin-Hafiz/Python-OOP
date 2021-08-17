cd4d6c93564ab3ae058266d7115581a02c4347a6
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2021-08-17 15:54:43 +0600

    introduction to duck typing

---
+++ polymorphism/Introduciton.py
@@ -0,0 +1,9 @@
+
+## polymorphism
+
+## poly = many
+# morph = form
+
+# that means one things but multiple forms
+
+## in oop object can be multiple forms is known as polymorphism
---
+++ polymorphism/duck_typing.py
@@ -0,0 +1,36 @@
+
+class PyCharm:
+
+    def execute(self):
+        print('run')
+        print('spell check')
+
+
+class MyEditor:
+
+    def execute(self):
+        print('run')
+        print('check validation')
+        print('spell check')
+
+class IDE:
+
+    def code(self, obj):
+        obj.execute() # if there is object has method execute that it , it never mind which class object it is
+        # this concept is known as duck typing.
+
+obj1 = MyEditor()
+obj2 = PyCharm()
+
+obj3 = IDE()
+
+print('my editor versions:')
+my_editor = obj3.code(obj1)
+print('pycharm  versions:',)
+py_charm =obj3.code(obj2)
+
+
+
+
+
+
